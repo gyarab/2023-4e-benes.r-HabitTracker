5be140b0c4f2457f07437903dfb8d59f35f5e438
RoryBenes <83403384+RoryBenes@users.noreply.github.com>
2024-03-22 21:51:39 +0100

    NovejHabitChaos

--- App.js
+++ App.js
@@ -10,40 +10,43 @@ import {
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useState, useEffect } from "react";
+import { NativeBaseProvider, Select, Input } from "native-base";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [tasks, setTasks] = useState([]);
+  const [habits, setHabits] = useState([]);
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" options={{ title: "Home" }}>
-          {({navigation}) => (<HomeScreen navigation={navigation} tasks={tasks}/>)}
-        </Stack.Screen>
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="NewTask">
-          {({navigation}) => (<NewTaskScreen navigation={navigation} tasks={tasks} setTasks={setTasks}/>)}
-        </Stack.Screen>
-        <Stack.Screen name="TaskDetail" component={TaskDetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" options={{ title: "Home" }}>
+            {({navigation}) => (<HomeScreen navigation={navigation} habits={habits}/>)}
+          </Stack.Screen>
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="NewHabit">
+            {({navigation}) => (<NewHabitScreen navigation={navigation} habits={habits} setHabits={setHabits}/>)}
+          </Stack.Screen>
+          <Stack.Screen name="HabitDetail" component={HabitDetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
-const HomeScreen = ({ navigation, tasks }) => {
+const HomeScreen = ({ navigation, habits }) => {
   return (
     <ScrollView>
       <Text>Home screen</Text>
-      {tasks.map((r, i) => (
-        <Task name={r.name} navigation={navigation} key={i}></Task>
+      {habits.map((r, i) => (
+        <Habit name={r.name} navigation={navigation} key={i}></Habit>
       ))}
       <StatusBar style="auto" />
       <Button
         title="Go to profile"
         onPress={() => navigation.navigate("Profile")}
       />
-      <Button title="New Task" onPress={() => navigation.navigate("NewTask")} />
+      <Button title="New Habit" onPress={() => navigation.navigate("NewHabit")} />
     </ScrollView>
   );
 };
@@ -51,26 +54,58 @@ const HomeScreen = ({ navigation, tasks }) => {
 const ProfileScreen = ({ navigation, route }) => {
   return (
     <View style={styles.container}>
-      <Text>Profile screen</Text>
+      <Text>wot u look'n at, eh?</Text>
       <StatusBar style="auto" />
     </View>
   );
 };
 
-const NewTaskScreen = ({ navigation, tasks, setTasks }) => {
-  const [text, setText] = React.useState("Useless Text");
+const NewHabitScreen = ({ navigation, habits, setHabits }) => {
+  const [text, setText] = React.useState("");
+  const [inputType, setInputType] = React.useState("");
+  const [nejakToRve, setNejakToRve] = React.useState("");
+
   return (
     <View style={styles.container}>
-      <Text>New Task screen</Text>
-      <TextInput
+      <Input
         style={styles.input}
         onChangeText={setText}
         value={text}
+        placeholder="Your new habit"
       />
+      <Select
+        placeholder="Choose input type"
+        selectedValue={inputType}
+        onValueChange={itemValue => setInputType(itemValue)}>
+          <Select.Item label="Amount (hours/kms/...)" value="amount" />
+          <Select.Item label="Yes/No (I have/n't done it)" value="yn" />
+      </Select>
+      <Text>
+        Set a goal!
+      </Text>
+      <Input
+        style={styles.input}
+        placeholder="amount"
+      />
+      <Input
+        style={styles.input}
+        placeholder="of what (hrs/kms/...)"
+      />
+      <Text>
+        per...
+      </Text>
+      <Select
+        placeholder="Day/Week/Month"
+        selectedValue={nejakToRve}
+        onValueChange={itemValue => setNejakToRve(itemValue)}>
+          <Select.Item label="Day"/>
+          <Select.Item label="Week"/>
+          <Select.Item label="Month"/>
+      </Select>
       <Button
         title="Save"
         onPress={() => {
-          setTasks([...tasks, { name: text }])
+          setHabits([...habits, { name: text }])
           navigation.navigate("Home");
         }}
       />
@@ -79,7 +114,7 @@ const NewTaskScreen = ({ navigation, tasks, setTasks }) => {
   );
 };
 
-const TaskDetailScreen = ({ navigation, route }) => {
+const HabitDetailScreen = ({ navigation, route }) => {
   const { name } = route.params;
   return (
     <View style={styles.container}>
@@ -89,14 +124,14 @@ const TaskDetailScreen = ({ navigation, route }) => {
   );
 };
 
-const Task = ({ navigation, route, name }) => {
+const Habit = ({ navigation, route, name }) => {
   return (
     <View>
-      <Text>Task {name}</Text>
+      <Text>Habit {name}</Text>
       <StatusBar style="auto" />
       <Button
-        title="Go to task detail"
-        onPress={() => navigation.navigate("TaskDetail", { name: name })}
+        title="Go to habit detail"
+        onPress={() => navigation.navigate("HabitDetail", { name: name })}
       />
     </View>
   );
